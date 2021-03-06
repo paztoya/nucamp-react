4dc214bb285956eccdca71ab8305acfd509ea15e
paztoya <daniel@paztoya.com>
2021-03-06 14:28:25 -0500

    wkshp wk2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
+import Campsite from "./CampsiteInfoComponent";
 // import Breadcrumb from "reactstrap/lib/Breadcrumb";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Directory extends Component {
   constructor(props) {
@@ -19,21 +13,6 @@ class Directory extends Component {
 
   onCampsiteSelect(campsite) {
     this.setState({ selectedCampsite: campsite });
-  }
-
-  renderSelectedCampsite(campsite) {
-    if (campsite) {
-      return (
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    return <div />;
   }
 
   render() {
@@ -53,11 +32,7 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
-          <div className="col-md-5 m-1">
-            {this.renderSelectedCampsite(this.state.selectedCampsite)}
-          </div>
-        </div>
+        <Campsite campsite={this.state.selectedCampsite} />
       </div>
     );
   }
