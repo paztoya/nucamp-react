ca0c6f450be3a4ea75c30e26146b33654a93a3e9
paztoya <daniel@paztoya.com>
2021-03-13 12:00:37 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Contact from "./ContactComponent";
 import { COMMENTS } from "../shared/comments";
@@ -35,6 +36,20 @@ class Main extends Component {
         />
       );
     };
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
 
     return (
       <div>
@@ -47,6 +62,7 @@ class Main extends Component {
             render={() => <Directory campsites={this.state.campsites} />}
           />
           <Route exact path="/contactus" component={Contact} />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
